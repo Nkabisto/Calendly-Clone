2439749bb3c380d9e4a6b28f44249dfd032bca79
Lionel <nkabisto@gmail.com>
2026-01-12 01:53:04 +0200

    Completed schema.ts

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -31,3 +31,32 @@ export const EventTable = pgTable(
     index("clerkUserIdIndex").on(table.clerkUserId), // index on clerkUserId for faster querying
   ])
   ,)
+
+// Define the "schedules" table, one per user, with timezone and timestamps
+export const ScheduleTable = pgTable("schedules", {
+  id: uuid("id").primaryKey().defaultRandom(),        // primary key with random UUID
+  timezone: text("timezone").notNull(),               // user's timezone
+  clerUserId: text("clerkUserId").notNull().unique(), // unique user ID from Clerk
+  createdAt,                                          // when the schedule was created
+  updatedAt,                                          // when the schedule was las updated
+})
+
+// Define a PostgreSQL ENUM for the days of the week
+export const scheduleDayOfWeekEnum = pgEnum("day", DAYS_OF_WEEK_IN_ORDER)
+// Define the "scheduleAvailabilities" table, which stores available time slots per day
+
+export const scheduleAvailabilityTable = pgTable(
+  "scheduleAvailabilities",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),      // unique ID
+    scheduleId: uuid("scheduleId")  // foreign key to the Schedule table
+      .notNull()
+      .references(()=> ScheduleTable.id, { onDelete: "cascade"}), // cascade delete when schedule is deleted
+    startTime: text("startTime").notNull(), // start time of availability (e.g. "09:00")
+    endTime: text("endTime").notNull(), // end time of availability (e.g. "17:00")
+    dayOfWeek: scheduleDayOfWeekEnum("dayOfWeek").notNull(),  // day of the week (ENUM)
+  },
+  table =>([
+    index("scheduleIdIndex").on(table.scheduleId),      // index on foreign key for faster lookups
+  ])
+)
